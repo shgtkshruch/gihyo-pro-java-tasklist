9f11160d23d2a7614de83ef4c3e17ae181be5cc5
shgtkshruch <shgtk.shruch@gmail.com>
2022-07-20 20:55:52 +0900

    Add HomeRest Controller

---
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -0,0 +1,17 @@
+package jp.gihyo.projava.tasklist;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+
+@RestController
+public class HomeRestController {
+  @RequestMapping("/resthello")
+  String hello() {
+    return """
+        Hello.
+        It works.
+        現在時刻は%sです。
+        """.formatted(LocalDateTime.now());
+  }
+}
