ad5ddd2584884d3466bea7ac998cb6064b256900
shgtkshruch <shgtk.shruch@gmail.com>
2022-07-26 09:51:55 +0000

    Use Thymeleaf template in HelloController

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -3,23 +3,14 @@ package jp.gihyo.projava.tasklist;
 import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
   @RequestMapping(value="/hello")
-  @ResponseBody
-  String hello() {
-    return """
-        <html>
-          <head><title>Hello</title></head>
-          <body>
-            <h1>Hello</h1>
-            It works!<br>
-            現在時刻は%sです。
-          </body>
-        </html>
-        """.formatted(LocalDateTime.now());
+  String hello(Model model) {
+    model.addAttribute("time", LocalDateTime.now());
+    return "hello";
   }
 }
