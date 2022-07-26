a6a67f7a4e17490484e135eaf8bac59c7447a29a
shgtkshruch <shgtk.shruch@gmail.com>
2022-07-26 09:24:03 +0000

    Add TaskItem record

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -3,13 +3,19 @@ package jp.gihyo.projava.tasklist;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HomeRestController {
+
+  record TaskItem(String id, String task, String deadline, boolean done) {}
+  private List<TaskItem> taskItems = new ArrayList<>();
+
   @RequestMapping("/resthello")
   String hello() {
     return """
-        Hello.
+        Hello!!
         It works.
         現在時刻は%sです。
         """.formatted(LocalDateTime.now());
