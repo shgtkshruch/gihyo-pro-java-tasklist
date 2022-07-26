2dc344f132c6e8422b034d224b9e5437720dfccf
shgtkshruch <shgtk.shruch@gmail.com>
2022-07-26 09:31:23 +0000

    Add `restadd` endpoint to add task

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -1,10 +1,13 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class HomeRestController {
@@ -15,9 +18,18 @@ public class HomeRestController {
   @RequestMapping("/resthello")
   String hello() {
     return """
-        Hello!!
+        Hello.
         It works.
         現在時刻は%sです。
         """.formatted(LocalDateTime.now());
+  }
+
+  @GetMapping("/restadd")
+  String addItem (@RequestParam("task") String task,
+                  @RequestParam("deadline") String deadline) {
+    String id = UUID.randomUUID().toString().substring(0, 8);
+    TaskItem item = new TaskItem(id, task, deadline, false);
+    taskItems.add(item);
+    return "タスクを追加しました";
   }
 }
